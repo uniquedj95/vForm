6d20b8e2f3e6baaaee5d440ff78b48a71cb88bf5
Daniel Justin <danieljmuleke@gmail.com>
2024-07-11 15:22:22 +0200

    Renamed the package from formique to vform for simplicity

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { App } from 'vue';
-import Form from './components/FormBuilder.vue';
+import { App, Plugin } from 'vue';
+import FormBuilder from './components/FormBuilder.vue';
 import TextInput from './components/inputs/TextInput.vue';
 
-export * from "./types";
-
-export default {
-  install(app: App) {
-    app.component('FormBuilder', Form);
-    app.component('TextInput', TextInput);
-  }
+export const VForm: Plugin = {
+    install(app: App) {
+        app.component('VForm', FormBuilder);
+        app.component('TextInput', TextInput);
+    }
 }
+
+export * from "./types";
