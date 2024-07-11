e01dbb882dfd23d1e4294f5513f3b65411aa37e5
Daniel Justin <danieljmuleke@gmail.com>
2024-07-11 10:52:54 +0200

    Refactor: Prefer default ionic styles and structure

--- src/components/inputs/TextInput.vue
+++ src/components/inputs/TextInput.vue
@@ -1,122 +1,56 @@
 <template>
-  <ion-label class="ion-padding-bottom bold" v-if="model.label">
-    <span>{{ model.label }}</span>
-    <span v-if="model.required" class="text-danger"> *</span>
-    <span
-      class="ion-float-right ion-margin-end checkbox-label"
-      v-if="model.allowUnknown"
-    >
-      {{ model.label }} Unknown?
-      <ion-checkbox v-model="isUnknown"></ion-checkbox>
-    </span>
-  </ion-label>
-  <div
-    class="ion-margin-top outer-input-wrapper"
-    :class="model.error ? 'box-input-error' : 'box-input'"
+  <ion-input
+    ref="inputRef"
+    v-model="input"
+    :clear-input="true"
+    :fill="model.fill ?? 'outline'"
+    :label-placement="model.labelPlacement ?? 'stacked'"
+    :label="model.label"
+    :required="model.required"
+    :error-text="model.error"
+    :auto-focus="model.autoFocus"
+    :placeholder="model.placeholder"
+    :disabled="model.disabled"
+    :counter="model.counter"
+    :max-length="model.maxLength"
+    :min-length="model.minLength"
+    @ionFocus="model.error = ''"
+    @ionBlur="validate"
+    @ionInput="validate"
   >
-    <div v-if="model.prefix" class="pre-suffix-wrapper">
-      <ion-label class="checkbox-label bold">{{ model.prefix }}</ion-label>
-    </div>
-    <div class="inner-input-wrapper">
-      <ion-input
-        class="ion-no-margin ion-no-padding"
-        v-model="model.value"
-        :placeholder="model.placeholder"
-        :disabled="model.disabled || isUnknown"
-        :autoFocus="model.autoFocus"
-        @ionFocus="model.error = ''"
-        @ionBlur="validate"
-      />
-    </div>
-    <div v-if="model.suffix" class="pre-suffix-wrapper">
-      <ion-label class="checkbox-label bold ion-float-right">
-        {{ model.suffix }}
-      </ion-label>
-    </div>
-  </div>
-  <ion-note v-if="model.error" color="danger">{{ model.error }}</ion-note>
+    <ion-label v-if="model.prefix" slot="start">{{ model.prefix }}</ion-label>
+    <ion-label v-if="model.suffix" slot="end">{{ model.suffix }} </ion-label>
+  </ion-input>
 </template>
 
 <script lang="ts" setup>
-import { IonCheckbox, IonInput, IonLabel, IonNote } from "@ionic/vue";
+import { IonInput, IonLabel } from "@ionic/vue";
 import { FormField, FormSchema } from "types";
-import { onMounted, PropType, ref, watch } from "vue";
+import { PropType, ref } from "vue";
 
 const props = defineProps<{ schema?: FormSchema }>();
 const model = defineModel({ type: Object as PropType<FormField>, default: {}});
+const inputRef = ref<typeof IonInput | null>(null);
+const input = ref(model.value.value as string);
 
-const isUnknown = ref(false);
-
-const validate = async () => {
-  if (model.value.required && !model.value.value) {
-    return (model.value.error = "This field is required");
-  }
-  if (
-    (!model.value.allowUnknown && model.value.value === "Unknown") ||
-    model.value.value === "N/A"
-  ) {
-    return (model.value.error = "Unknown is not allowed");
+async function validate() {
+  inputRef.value?.$el.classList.remove("ion-invalid");
+  inputRef.value?.$el.classList.remove("ion-valid");
+  if ((!model.value.allowUnknown && input.value === "Unknown") || input.value === "N/A") {
+    model.value.error = "Unknown is not allowed";
+    inputRef.value?.$el.classList.add("ion-invalid");
+    return; 
   }
   if (model.value.validation) {
-    const errors = await model.value.validation(
-      model.value.value!,
-      props?.schema
-    );
+    const errors = await model.value.validation(input.value, props?.schema);
     if (errors && errors.length) {
-      return (model.value.error += errors.toString());
+      model.value.error = errors.toString();
+      inputRef.value?.$el.classList.add("ion-invalid");
+      return
     }
   }
-  return (model.value.error = "");
+  model.value.error = "";
+  model.value.value = input.value;
+  inputRef.value?.$el.classList.add("ion-valid");
 };
-
-watch(isUnknown, (newValue) => {
-  if (newValue) {
-    model.value.value = "Unknown";
-    model.value.error = "";
-    model.value.disabled = true;
-  } else {
-    model.value.value = "";
-    model.value.disabled = false;
-  }
-});
-
-watch(() => model.value, (newModel) => {
-  if (newModel.value === "Unknown") {
-    if (!isUnknown.value) {
-      isUnknown.value = true;
-    }
-  } else {
-    isUnknown.value = false;
-  }
-});
-
-onMounted(() => {
-  if (model.value.value === "Unknown" && !isUnknown.value) {
-    isUnknown.value = true;
-  }
-});
 </script>
-
-<style>
-.outer-input-wrapper {
-  width: 100%;
-  height: 2.5rem;
-  display: flex;
-  justify-content: space-between;
-  justify-items: center;
-}
-
-.inner-input-wrapper {
-  background: #ffffff;
-  height: 100%;
-  padding-top: 0 !important;
-  flex-grow: 8;
-}
-
-.pre-suffix-wrapper {
-  background: #f2f2f2;
-  height: 100%;
-  padding: 0.5rem;
-  flex-grow: 1;
-}
-</style>
