b3eac81ed5a8d988b7488cdd5c719e2892bde23e
Daniel Justin <danieljmuleke@gmail.com>
2024-07-12 10:36:14 +0200

    Register additional input fields to the plugin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { App, Plugin } from 'vue';
 import FormBuilder from './components/FormBuilder.vue';
 import TextInput from './components/inputs/TextInput.vue';
+import DateInput from './components/inputs/DateInput.vue';
+import NumberInput from './components/inputs/NumberInput.vue';
+import EmailInput from './components/inputs/EmailInput.vue';
+import PasswordInput from './components/inputs/PasswordInput.vue';
 
 export const VForm: Plugin = {
     install(app: App) {
         app.component('VForm', FormBuilder);
         app.component('TextInput', TextInput);
+        app.component('DateInput', DateInput);
+        app.component('NumberInput', NumberInput);
+        app.component('EmailInput', EmailInput);
+        app.component('PasswordInput', PasswordInput);
     }
 }
 
