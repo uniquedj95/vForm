c925c0a646fb16c3536bd12ab20dbb1a98b5144b
Daniel Justin <danieljmuleke@gmail.com>
2024-11-01 13:37:06 +0200

    Added search functionality

--- src/components/inputs/SelectInput.vue
+++ src/components/inputs/SelectInput.vue
@@ -1,75 +1,127 @@
 <template>
-  <div>
-    <ion-select
-      v-model="input"
+  <div class="autocomplete-container">
+    <ion-input
       ref="inputRef"
-      interface="popover"
-      :placeholder="model.placeholder"
-      :multiple="model.multiple"
-      :disabled="model.disabled"
-      :required="model.required"
-      :error-text="model.error"
-      :auto-focus="model.autoFocus"
+      v-model="filter"
       :clear-input="true"
       :fill="model.fill ?? 'outline'"
       :label-placement="model.labelPlacement ?? 'stacked'"
-      @ion-focus="onFocus"
-      @ion-change="onValueUpdate"
+      :type="type ?? 'text'"
+      :required="model.required"
+      :error-text="model.error"
+      :auto-focus="model.autoFocus"
+      :placeholder="placeholder"
+      :disabled="model.disabled"
+      :counter="model.counter"
       @ion-blur="onValueUpdate"
+      @ion-focus="onFocus"
+      :debounce="300"
     >
       <ion-label slot="label" v-if="model.label">
-        {{ model.label }} 
+        {{ model.label }}
         <ion-text color="danger" v-if="model.required">*</ion-text>
       </ion-label>
-      <ion-select-option
+      <ion-chip v-for="(tag, index) of tags" :key="index" slot="start">
+        <ion-label>{{ tag.label }}</ion-label>
+        <ion-icon
+          :icon="closeCircle"
+          color="danger"
+          @click="diselect(tag)"
+          style="z-index: 20"
+        ></ion-icon>
+      </ion-chip>
+      <ion-icon slot="end" :icon="chevronDown" />
+    </ion-input>
+
+    <ion-list v-if="showOptions && options.length > 0" class="suggestions-list">
+      <ion-item
         v-for="option in options"
-        :key="option.value"
-        :value="option.value"
+        button
+        :key="option.label"
+        @click="onSelect(option)"
       >
-        {{ option.label }}
-      </ion-select-option>
-    </ion-select>
-    <ion-note v-if="model.error" color="danger" class="ion-padding-start">
-      {{ model.error }}
-    </ion-note>
+        <ion-checkbox
+          slot="start"
+          :checked="option.isChecked"
+          v-if="model.multiple"
+        />
+        <ion-label>{{ option.label }}</ion-label>
+      </ion-item>
+    </ion-list>
   </div>
 </template>
 
 <script setup lang="ts">
-import { IonSelect, IonSelectOption, IonNote, IonLabel, IonText } from "@ionic/vue";
-import { FormField, FormSchema, Option } from "types";
-import { getModelValue, isEmpty, mapValueToOption } from "../../utils";
-import { PropType, ref, watch } from "vue";
+import { ref, computed, PropType, watch } from "vue";
+import { chevronDown, closeCircle } from "ionicons/icons";
+import { FormSchema, BaseFieldTypes, FormField, Option } from "types";
+import { isEmpty, checkOption, getFilteredOptions, findOption, uncheckOption } from "../../utils";
+import {
+  IonInput,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonChip,
+  IonText,
+  IonIcon,
+  IonCheckbox,
+} from "@ionic/vue";
 
-const props = defineProps<{ schema?: FormSchema }>();
+const props = defineProps<{ schema?: FormSchema; type?: BaseFieldTypes }>();
 const model = defineModel({ type: Object as PropType<FormField>, default: {} });
-const inputRef = ref<typeof IonSelect | null>(null);
-const input = ref(getModelValue(model.value));
-const options = ref<Array<Option>>([]);
+const inputRef = ref<typeof IonInput | null>(null);
+const showOptions = ref(false);
+const options = ref<Option[]>([]);
+const filter = ref("");
 
-watch(() => model.value.value, () => input.value = getModelValue(model.value));
-  
-watch(() => model.value.options, async () => {
-  if(typeof model.value.options === "function") {
-    options.value = await model.value.options();
-  } else {
-    options.value = model.value.options ?? [];
-  }
-}, 
-{
-  immediate: true, 
-  deep: true
+const tags = computed<Option[]>(() =>
+  options.value.filter(({ isChecked }) => isChecked)
+);
+
+const placeholder = computed(() => {
+  return !filter.value && isEmpty(tags.value) && !showOptions.value
+    ? model.value.placeholder ?? "Select an option"
+    : "";
 });
 
+watch(() => model.value.options, filterOptions, { immediate: true, deep: true });
+watch(() => model.value.value, initialize, { immediate: true, deep: true });
+
+function onReset() {
+  options.value.forEach(diselect);
+  model.value.error = "";
+  model.value.value = model.value.multiple ? [] : "";
+}
+
+function diselect(tag: Option) {
+  const option = options.value.findIndex((o) => o.value === tag.value);
+  if (option) options.value[option].isChecked = false;
+}
+
+function onSelect(item: Option) {
+  if (item.isChecked) uncheckOption(item, options.value);
+  else {
+    checkOption(item, options.value);
+    if (!model.value.multiple) onValueUpdate();
+  }
+}
+
+function onFocus() {
+  inputRef.value?.$el.classList.remove("ion-touched");
+  inputRef.value?.$el.classList.remove("ion-invalid");
+  model.value.error = "";
+  showOptions.value = true;
+}
+
 async function isValid() {
-  if (model.value.required && isEmpty(input.value)) {
+  if (model.value.required && isEmpty(tags.value)) {
     model.value.error = "This field is required";
     return false;
   }
-  if (typeof model.value.validation === "function") {
-    const errors = await model.value.validation(input.value as string, props?.schema);
+  if (model.value.validation) {
+    const errors = await model.value.validation(tags.value, props?.schema);
     if (errors && errors.length) {
-      model.value.error = errors.toString();
+      model.value.error = errors.join();
       return false;
     }
   }
@@ -77,33 +129,74 @@ async function isValid() {
 }
 
 async function onValueUpdate() {
-  inputRef.value?.$el.classList.remove("ion-valid", "ion-invalid");
+  showOptions.value = false;
+  inputRef.value?.$el.classList.remove("ion-invalid");
+  inputRef.value?.$el.classList.remove("ion-valid");
 
   if (await isValid()) {
     model.value.error = "";
-    model.value.value = mapValueToOption(input.value, options.value, model.value.multiple);
+    model.value.value = model.value.multiple ? tags.value : tags.value[0];
     inputRef.value?.$el.classList.add("ion-valid");
   } else {
     inputRef.value?.$el.classList.add("ion-invalid");
   }
+
   inputRef.value?.$el.classList.add("ion-touched");
 }
 
-function onFocus() {
-    inputRef.value?.$el.classList.remove("ion-touched");
-    inputRef.value?.$el.classList.remove("ion-invalid");
-    model.value.error = "";
+async function filterOptions() {
+  const filtered =
+    typeof model.value.options === "function"
+      ? await model.value.options(filter.value)
+      : getFilteredOptions(model.value.options ?? [], filter.value);
+
+  tags.value.forEach((tag) => checkOption(tag, filtered));
+  options.value = filtered.filter((o) => !!o.label);
 }
 
-function onReset() {
-  input.value = model.value.multiple ? [] : "";
-  model.value.error = "";
-  model.value.value = "";
+function initialize() {
+  onReset();
+  const defaultValue = model.value.value;
+  if (defaultValue) {
+    if (Array.isArray(defaultValue)) {
+      defaultValue.forEach((opt) => checkOption(opt, options.value));
+    } else if (typeof defaultValue === "object") {
+      checkOption(defaultValue, options.value);
+    } else {
+      checkOption(
+        {
+          value: defaultValue as any,
+          label: defaultValue as string,
+        },
+        options.value
+      );
+    }
+  }
 }
 
 defineExpose({
-  onValueUpdate, 
+  onValueUpdate,
   onReset,
-  getErrors: () => model.value.error 
+  getErrors: () => model.value.error,
 });
 </script>
+
+<style scoped>
+.autocomplete-container {
+  position: relative;
+}
+
+.suggestions-list {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  right: 0;
+  background-color: white;
+  border: 1px solid #ddd;
+  z-index: 99000;
+  max-height: 65vh;
+  overflow-y: auto;
+  border-radius: 0 0 5px 5px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
