07905a9bd816029423b2a87be1af62b52f04d993
Daniel Justin <danieljmuleke@gmail.com>
2025-06-09 00:57:58 +0200

    feat: refactor input components to use BaseInput and remove input component factory

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     coverage: {
-      provider: 'c8',
+      provider: 'v8',
       reporter: ['text', 'json', 'html'],
     },
   },
