450db4a6e5424c02499ec59b1f89b2f0a8a15ec5
Daniel Justin <danieljmuleke@gmail.com>
2025-07-28 18:16:53 +0200

    feat: remove unused imports from CustomComponent for cleaner code

--- demo/src/components/CustomComponent.vue
+++ demo/src/components/CustomComponent.vue
@@ -99,7 +99,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, defineEmits, defineExpose } from 'vue';
+import { ref, onMounted } from 'vue';
 import {
   IonCard,
   IonCardHeader,
